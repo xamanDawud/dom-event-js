f5afd5ef29cb843f6578d176fc96cc327de35ed0
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-14 16:57:52 +0600

    Dom Event handler 4 ways explored

--- event.js
+++ event.js
@@ -1,1 +1,19 @@
-console.log('Hello Events');
+console.log("Hello Events");
+
+function makeGreen() {
+    document.body.style.backgroundColor = "green";
+}
+
+// 3rd way
+let red = document.getElementById("red");
+red.onclick = makeRed;
+
+function makeRed() {
+    document.body.style.backgroundColor = "red";
+}
+
+// 4th way
+let purple = document.getElementById("purple");
+purple.onclick = function() {
+    document.body.style.backgroundColor = "purple";
+};
